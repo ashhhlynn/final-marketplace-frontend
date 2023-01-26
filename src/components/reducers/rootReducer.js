aa18fd690ca738b93f7dc80951dbb922b4f722de
ashhhlynn <ashleylynnwelch@gmail.com>
2023-01-25 21:53:56 -0600

    reducer actions, adding reducer stuff to index.js

--- src/components/reducers/rootReducer.js
+++ src/components/reducers/rootReducer.js
@@ -1,12 +1,42 @@
 
 const initialState = {
-
     products: []
-   
 }
 
 const rootReducer = (state = initialState, action) => {
-    
+    switch (action.type) {
+    case "ADD_PRODUCT_REQUEST":
+        return {
+          ...state,
+          products: [...state.products],
+          loading: true,
+        };
+
+    case "ADD_PRODUCT":
+        return {
+        ...state,
+        products: action.data,
+        loading: false,
+        };
+
+    case "FETCH_PRODUCTS_REQUEST":
+            return {
+            ...state,
+            products: [...state.products],
+            loading: false,
+            };
+
+    case "FETCH_PRODUCTS":
+        console.log(action.products)
+        return {
+        ...state,
+        products: action.products,
+        loading: false,
+        };
+
+    default:
+        return state;
+    }
 }
 
 export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import 'semantic-ui-css/semantic.min.css'
 import rootReducer from './components/reducers/rootReducer';
+import thunk from 'redux-thunk';
+import { applyMiddleware } from 'redux';
+
+
+const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Provider store={store}>
     <App />
- 
+    </Provider>
   </React.StrictMode>)
 
 
