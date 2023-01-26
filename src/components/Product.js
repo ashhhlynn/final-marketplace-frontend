97ac7897164a91b2afe0a258862e1d8fdda558ca
ashhhlynn <ashleylynnwelch@gmail.com>
2023-01-25 21:28:22 -0600

    made product component, added to product container

---
+++ src/components/Product.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Header, Card } from 'semantic-ui-react'
+
+
+
+class Product extends Component {
+
+    render() {
+        return{}
+    }
+
+}
+
+export default Product
--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import CreateProduct from './CreateProduct'
-import { Header } from 'semantic-ui-react'
+import { Header, Card } from 'semantic-ui-react'
+import Product from './Product'
 
 
 class ProductContainer extends Component {
-
     render() {
+        const products = this.props.products.map( i => {
+            return (
+                <Product product={i} key={i.id}/> 
+            )
+        })
         return (
-<div>    <Header as='h1' color='teal'><center>All Products:</center></Header>
-
-    <CreateProduct />
-    </div>
+        <div>
+        <Header as='h1' color='teal'><center>All Products:</center></Header>
+        <CreateProduct />
+        <Card.Group>
+        {products}
+        </Card.Group>
+        </div>
             )
         }
-
     }
 
 
