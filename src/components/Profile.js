ab90dfd0e36addd3812230e2a21e9c3bc52b1751
ashhhlynn <ashleylynnwelch@gmail.com>
2023-01-26 02:08:38 -0600

    building profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import { Segment, Card, List, Header } from 'semantic-ui-react'
+
+
+class Profile extends Component {
+
+   
+
+    render() {
+        
+        return (
+        <div>
+
+<Segment>
+<Header color='teal' size='huge'> 
+
+Hi {this.props.user.name}!</Header>
+<Header>User Info:</Header>
+
+        Email: {this.props.user.email}<br></br>
+    Address: {this.props.user.address}
+
+
+            </Segment>
+        </div>
+        )
+    }
+}
+
+const MSTP = (state) => {
+    return {
+        user: state.currentUser,
+    }
+}
+
+const MDTP = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(MSTP, MDTP)(Profile)
