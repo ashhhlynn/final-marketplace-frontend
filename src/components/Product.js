f2a9e8d437be8d5426427e489e577d937b6501c5
ashhhlynn <ashleylynnwelch@gmail.com>
2023-01-25 22:40:18 -0600

    rendering product component content

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import { Header, Card } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
 
 
 class Product extends Component {
 
-    render() {
-        return{}
+render(){
+    const i = this.props.product
+    return (
+                <Card centered key={i.id}>
+                     <Image 
+                    src={i.image_url}/>
+                    <Card.Header>{i.title}: ${i.price}</Card.Header>
+                <Card.Description>{i.description}</Card.Description>
+                <button className="addCartBtn"
+            onClick={()=>{this.handleAddCart(i.id, i.title, i.price)}}>Add to Cart</button> 
+            </Card>
+    )
     }
 
+    handleAddCart(id, title, price){
+
+
+    }    
 }
 
 export default Product
