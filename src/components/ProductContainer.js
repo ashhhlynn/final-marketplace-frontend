f86f2eebb06cf245b8912321be1586da4b611fc0
ashhhlynn <ashleylynnwelch@gmail.com>
2023-01-25 20:09:24 -0600

    put header in product container

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import CreateProduct from './CreateProduct'
+import { Header } from 'semantic-ui-react'
 
 
 class ProductContainer extends Component {
 
     render() {
         return (
-<div>Hi
+<div>    <Header as='h1' color='teal'><center>All Products:</center></Header>
+
     <CreateProduct />
     </div>
             )
