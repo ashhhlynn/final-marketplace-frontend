44ccded9778d5f032864ba3c542f8970ed00b637
ashhhlynn <ashleylynnwelch@gmail.com>
2023-04-18 02:08:31 -0500

    test

--- src/components/actions/productActions.js
+++ src/components/actions/productActions.js
@@ -1,7 +1,7 @@
 export const fetchProducts = () => {
     return (dispatch) => {
         dispatch({ type: "FETCH_PRODUCTS_REQUEST" })
-        fetch("https://final-marketplace-api/products.onrender.com")
+        fetch("https://final-marketplace-api.onrender.com/products")
             .then(response => response.json())
             .then(products => {dispatch({ type: "FETCH_PRODUCTS", products })
         })
