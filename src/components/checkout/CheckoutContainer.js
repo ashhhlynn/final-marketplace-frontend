c2799dc554c677081d5670f369559b8839220682
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-31 09:23:20 -0500

    test

--- src/components/checkout/CheckoutContainer.js
+++ src/components/checkout/CheckoutContainer.js
@@ -48,7 +48,7 @@ class CheckoutContainer extends Component {
                                 color="black"
                                 style={{backgroundColor: "black", floated:"right", marginLeft:"13%"}}
                                 token={onToken}
-                                stripeKey={process.env.STRIPE_PUBLISHABLE_KEY}
+                                stripeKey={env['STRIPE_PUBLISHABLE_KEY']}
                                 className="checkout"
                                 />
                             </h2>
