ca2368ea08df90c2764b5ff1670f1a65fb7c1804
ashhhlynn <ashleylynnwelch@gmail.com>
2023-12-08 23:39:24 -0600

    styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Segment } from 'semantic-ui-react'
 
 const Footer= () => {
     return (
-        <Segment size="tiny" style={{marginTop: "0%", borderBox:"1", borderColor:"black",  background:"#fdfcf9", height:"60px"}}>
+        <Segment size="tiny" style={{marginTop: "0%", borderBox:"1", borderColor:"#f0f0f0",  background:"#fdfcf9", height:"60px"}}>
             <center> 
                 <h5 style={{marginTop:"1%", color:"#26453e"}}> © 2023 Seedlink</h5>
             </center>
